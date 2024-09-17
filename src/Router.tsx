0ef44da3aca0d44617e5df15648403279fbe3220
daehyeong2 <baconbacon1231@gmail.com>
2024-09-18 04:13:12 +0900

    errorElement (15.3)

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,15 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
       },
       {
         path: "about",
