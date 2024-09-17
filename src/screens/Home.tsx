c27f9fdd600de2286f04c25647912a43a8a81627
daehyeong2 <baconbacon1231@gmail.com>
2024-09-18 04:16:33 +0900

    useNavigate (15.4)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  const users: any = [];
-  return <h1>{users.dff.f}</h1>;
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate("/about");
+  }, 3000);
+  return (
+    <h1>
+      Home<button onClick={() => navigate("/about")}>Go To About</button>
+    </h1>
+  );
 };
 
 export default Home;
